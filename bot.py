0a656bf32fa772ee3b1317448126f24f5a6e8cfe
Aksa Rose <117498997+AksaRose@users.noreply.github.com>
2025-08-20 15:11:44 +0530

    deploy: give enought debugg

--- bot.py
+++ bot.py
@@ -22,7 +22,7 @@ logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO
 )
 # set higher logging level for httpx to avoid all GET and POST requests being logged
-logging.getLogger("httpx").setLevel(logging.WARNING)
+logging.getLogger("httpx").setLevel(logging.INFO)
 
 logger = logging.getLogger(__name__)
 
