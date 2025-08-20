2528c466bc66ba2533791adb2448c8af0884d565
Aksa Rose <117498997+AksaRose@users.noreply.github.com>
2025-08-20 20:54:24 +0530

    build: cors

--- api.py
+++ api.py
@@ -14,9 +14,7 @@ from fastapi.middleware.cors import CORSMiddleware # Import CORSMiddleware
 app = FastAPI()
 
 origins = [
-    "http://localhost",
-    "http://localhost:5173", # React development server
-    "http://127.0.0.1:5173", # Local IP for React development server
+    "https://web-production-d161.up.railway.app",
 ]
 
 app.add_middleware(
